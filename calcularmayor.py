f86c9ac5cffa3e7945566c529b3407cd3a411dd1
marcelotimana <87838359+marcelotimana@users.noreply.github.com>
2021-10-13 20:57:02 -0500

    Add files via upload

---
+++ calcularmayor.py
@@ -0,0 +1,12 @@
+numero=int(input("ingrese un numero:"))
+numero1=int(input("ingrese un numero:"))
+numero2=int(input("ingrese un numero:"))
+
+if numero > numero1 and numero>numero2:
+    print("el numero mayor es:",numero)
+
+elif numero1>numero and numero1>numero2:
+    print("el numero mayor es:",numero1)
+    
+elif numero2>numero and numero2>numero1:
+    print("el numero mayor es:",numero2)
